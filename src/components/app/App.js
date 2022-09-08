902923c0cfe3aa973035f102dac004a362e7fa39
mmerguoum <merguoummourad@gmail.com>
2022-09-08 15:04:05 +0000

    create cards and delete card and button show and hide

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,34 +4,35 @@ import Cards from '../card/Cards';
 
 
 function App() {
-  const [ showToggel, setShowToggel ] = useState(false)
-  const [ state, setState ] = useState([
+  const [showToggel, setShowToggel] = useState(false)
+  const [state, setState] = useState([
     { name: "Mourad", age: "33 ans", adress: "Bettana", phone: "0653866554"},
     { name: "Mouhcine", age: "40 ans", adress: "Hay salam", phone: "0653866555"},
     { name: "Rayan", age: "10 ans", adress: "Medina", phone: "0653866556"}
   ])
 
-  const handleDelete = (e, idx) => {
-    setState((prevState) => {
-     return prevState.filter((el,index) => index !== idx)
-  })
-  console.log(state)
-}
-
   const handleToggel = () => {
-  setShowToggel(!showToggel)
-}
+    setShowToggel(!showToggel)
+  }
+
+  const handleDeleteStudent = (e, idx) => {
+      setState((prevState) => {
+        return prevState.filter((e,index) => index !== idx)  
+      }
+      )
+      
+      console.log(state)
+  }
 
   return (
     <div className="App">
       <h1 className='grandTitle'>List Students</h1>
-      <button
-        style={{marginBottom: "20px"}}
-        onClick={() => handleToggel()}>
-        {!showToggel ? "Show List Students" : "Hide List Students"}
+      <button style={{marginBottom: "20px"}}
+      onClick={handleToggel}
+      >{showToggel ? "Show List Students" : "Hide List Students"}
       </button>
-      <div className={showToggel ? "show" : "hide"}>
-          <Cards students={state} deleteStudent={handleDelete}/>
+      <div className={!showToggel ? "show" : "hide"}>
+          <Cards students={state} handleDeleteStudent={handleDeleteStudent}/>
       </div>
     </div>
   );
